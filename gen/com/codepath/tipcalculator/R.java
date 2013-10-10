bfd444d4ae39bfab0bae71bfcdcdce61f6efe079
Jade McGough <jade@thezets.com>
2013-10-10 03:44:46 -0700

    use seekbar instead of buttons

--- gen/com/codepath/tipcalculator/R.java
+++ gen/com/codepath/tipcalculator/R.java
@@ -24,13 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnFifteenPct=0x7f080002;
-        public static final int btnTenPct=0x7f080003;
-        public static final int btnTwentyPct=0x7f080001;
+        public static final int action_settings=0x7f080005;
         public static final int etTransaction=0x7f080000;
-        public static final int tvTip=0x7f080004;
-        public static final int tvTipValue=0x7f080005;
+        public static final int sbTipPercent=0x7f080003;
+        public static final int tvTip=0x7f080001;
+        public static final int tvTipPercent=0x7f080004;
+        public static final int tvTipValue=0x7f080002;
     }
     public static final class layout {
         public static final int activity_calculator=0x7f030000;
--- src/com/codepath/tipcalculator/CalculatorActivity.java
+++ src/com/codepath/tipcalculator/CalculatorActivity.java
@@ -1,19 +1,21 @@
 package com.codepath.tipcalculator;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
-import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CalculatorActivity extends Activity {
 	private EditText etTransaction;
 	private TextView tvTipValue;
+	private SeekBar sbTipPercent;
+	private TextView tvTipPercent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +23,75 @@ public class CalculatorActivity extends Activity {
         setContentView(R.layout.activity_calculator);
         etTransaction = (EditText) findViewById(R.id.etTransaction);
         tvTipValue = (TextView) findViewById(R.id.tvTipValue);
+        sbTipPercent = (SeekBar) findViewById(R.id.sbTipPercent);
+        tvTipPercent = (TextView) findViewById(R.id.tvTipPercent);
         
         etTransaction.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        
+        addActionListeners();
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.calculator, menu);
         return true;
     }
-    
-    public void showTip(View v) {
-    	float transaction;
+
+    private void addActionListeners() {
     	
-    	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-    	imm.hideSoftInputFromWindow(v.getWindowToken(), 0);    	
-    	
-    	try {
-    		transaction = Float.parseFloat(etTransaction.getText().toString());
-        	float percent = Float.parseFloat(v.getTag().toString());
-        	tvTipValue.setText("$" + String.format("%.2f", transaction * percent));
-    	} catch (NumberFormatException e) {
-    		Toast.makeText(getApplicationContext(), "Enter a number", Toast.LENGTH_SHORT).show();
-		}
-    	
+        sbTipPercent.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				tvTipPercent.setText(String.valueOf(progress) + '%');
+				
+				if (etTransaction.getText().toString().trim().length() > 0) {
+					updateTipValue();
+				}
+			}
+        });
+        
+        etTransaction.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				updateTipValue();
+			}
+		});
     }
     
+    private void updateTipValue() {
+		float transaction = Float.parseFloat(etTransaction.getText().toString());
+		float percent = sbTipPercent.getProgress();
+		tvTipValue.setText("$" + String.format("%.2f", transaction * percent / 100));
+    }
+        
 }
